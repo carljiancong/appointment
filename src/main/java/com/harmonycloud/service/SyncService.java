3261e19b4d9cc2ebddb71363b3b22ac8dab5ff12
余倩 <qianyu@harmonycloud.cn>
2019-03-21 23:10:26 +0800

    update

--- src/main/java/com/harmonycloud/service/SyncService.java
+++ src/main/java/com/harmonycloud/service/SyncService.java
@@ -1,20 +1,15 @@
 package com.harmonycloud.service;
 
-import com.harmonycloud.dto.ResponseDto;
-import com.harmonycloud.exception.AppointmentException;
+import com.harmonycloud.result.CimsResponseWrapper;
 import com.harmonycloud.util.TraceUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 @Service
 public class SyncService {
@@ -25,7 +20,7 @@ public class SyncService {
     @Autowired
     private HttpServletRequest request;
 
-    public ResponseDto save(URI uri, String token, Object body) throws Exception{
+    public CimsResponseWrapper save(URI uri, String token, Object body) throws Exception{
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + token);
         if (request.getHeader("user") != null) {
@@ -38,7 +33,7 @@ public class SyncService {
         TraceUtil.addTraceForHttp(request, headers);
         TraceUtil.printTrace(headers);
         HttpEntity<Object> request = new HttpEntity<>(body, headers);
-        ResponseEntity<ResponseDto> response = restProxyTemplate.getRestTemplate().postForEntity(uri, request, ResponseDto.class);
+        ResponseEntity<CimsResponseWrapper> response = restProxyTemplate.getRestTemplate().postForEntity(uri, request, CimsResponseWrapper.class);
         return response.getBody();
     }
 }
